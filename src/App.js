98738251b55b51c1bc22225f6235a2bd5a6f9f14
jmokate <jmokate@gmail.com>
2022-03-21 20:08:39 -0500

    menu updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import {Navbar, Nav, Image, Container, Row, Col} from 'react-bootstrap'
+import {Navbar, Nav, Image, Container, Row, Col,} from 'react-bootstrap'
 import './App.css';
 import logo from "./Assets/Logo.png"
 
 function App() {
   return (
     <>
-    <Container fluid className="mr-5">
+    
+    <Container className=".container-md">
       <Row className="">
         <Col lg={6}>
-        <Image  className="nav-logo" src={logo}/> 
+        <img  className="nav-logo" src={logo}/> 
         </Col>   
         <Col lg={6} className="contactLink mx-auto">
           contact
