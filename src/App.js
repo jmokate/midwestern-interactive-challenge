5bfccedab2b4a58fc5c321103e9cf940d9b3e9c9
jmokate <jmokate@gmail.com>
2022-03-22 17:46:12 -0500

    start on cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import {Navbar, Nav, Image, Container, Row, Col,} from 'react-bootstrap'
+import {Navbar, Nav, Image, Container, Row, Col, Card, Button, CardGroup} from 'react-bootstrap'
 import './App.css';
 import logo from "./Assets/Logo.png"
+import talkie from "./Assets/Talkie.png"
 
 function App() {
   return (
@@ -15,6 +16,38 @@ function App() {
           contact
           </Col>
       </Row>
+      <Row style={{paddingTop: "10rem"}}>
+        <CardGroup>
+         <Card className="text-center" style={{ width: '25rem',height: "25rem", backgroundColor: "#F5F5F5", borderRadius: "1%" }}>
+           <Card.Img variant="top" src={talkie} />
+            <Card.Body>
+             <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+               Some quick example text to build on the card title and make up the bulk of
+               the card's content.
+              </Card.Text>
+             <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+
+          {/* <Card className="text-center" style={{ width: '25rem',height: "25rem", backgroundColor: "#F5F5F5", borderRadius: "1%" }}>
+           <Card.Img variant="top" src={talkie} />
+            <Card.Body>
+             <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+               Some quick example text to build on the card title and make up the bulk of
+               the card's content.
+              </Card.Text>
+             <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card> */}
+        </CardGroup>
+      
+
+
+
+      </Row>
+      
     </Container>
     
     </>
