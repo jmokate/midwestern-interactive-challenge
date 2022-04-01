a981a6ff8ee0a629a6cb83c9cf1102f42ed8b5f4
jmokate <jmokate@gmail.com>
2022-04-01 17:09:06 -0500

    commit for merge

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import react, {useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeMenu from './Components/HomeMenu.js'
 import Cards from './Components/Cards.js'
+import List from './Components/List.js'
 import {Container, Row, Col, Card, Button, Image} from 'react-bootstrap';
 import './App.css';
 import logo from "./Assets/Logo.png"
-//import Cards from './Cards.js'
 import talkie from "./Assets/Talkie.png"
 import rabbit from "./Assets/Rabbit.png"
 import shield from "./Assets/Shield.png"
@@ -20,11 +20,7 @@ console.log(usersArray, 'hi');
 
 function App() {
 
-  // const [isList, setList] = useState(false);
-  let isList = true;
-  const displayList = () => {
-    isList = true;
-  }
+ 
 
   return (
     <>
@@ -41,17 +37,9 @@ function App() {
          />
       </Container>
       <Container className="px-4 mt-5">
-        <Row>
-          <Col style={{}}>
-          <h1 style={{color: '#f5f5f5', fontSize: '50px', fontWeight: 'bold'}}>Heading One</h1>
-          <hr style={{color: '#debf79', width: "12rem", height: '5%', opacity: '100'}}/>
-          <p style={{color: '#f5f5f5'}} >Click here</p>
-          <ul style={{color: '#f5f5f5'}} onClick={displayList()}>
-
-            { isList ? usersArray.map(user =>  <li key={user}>{user}</li>) : 'something is weird'}
-          </ul>
-          </Col>
-        </Row>
+       
+        <List
+          usersArray={usersArray} />
         
       </Container>
         
