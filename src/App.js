c8c1f5a2b81eab62a70808abaf3876e121452d31
jmokate <jmokate@gmail.com>
2022-04-01 14:47:42 -0500

    start react router

--- src/App.js
+++ src/App.js
@@ -1,68 +1,23 @@
 import react, {useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HomeMenu from './Components/HomeMenu.js'
-import Cards from './Components/Cards.js'
-import {Container, Row, Col, Card, Button, Image} from 'react-bootstrap';
 import './App.css';
-import logo from "./Assets/Logo.png"
-//import Cards from './Cards.js'
-import talkie from "./Assets/Talkie.png"
-import rabbit from "./Assets/Rabbit.png"
-import shield from "./Assets/Shield.png"
-import usersArray from './Users.js'
+import Home from './Components/Home.js'
+import Cards from './Components/Cards.js'
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
-
-
-
-console.log(usersArray, 'hi');
 
 
 function App() {
-
- const [isList, setList] = useState(false);
- 
-
- function handleClick(event) {
-   if (isList) {alert("List is already displayed.")}
-   console.log(event)
-   setList(true)
-
- }
-
-  return (
-    <>
+  return(
     
-    <Container>
-      <HomeMenu src={logo} />
-    </Container>
-    
-      <Container className="px-4" style={{paddingTop: "3%"}}>
-        <Cards
-          talkie={talkie}
-          rabbit={rabbit}
-          shield={shield}
-         />
-      </Container>
-      <Container className="px-4 mt-5">
-        <Row>
-          <Col style={{}}>
-          <h1 style={{color: '#f5f5f5', fontSize: '50px', fontWeight: 'bold'}}>Heading One</h1>
-          <hr style={{color: '#debf79', width: "12rem", height: '.5rem', opacity: '100'}}/>
-          <p style={{color: '#f5f5f5'}} onClick={(e) => handleClick(e)} >Click here for the users list.</p>
-          <ul style={{color: '#f5f5f5'}} >
-
-             { isList ? usersArray.map(user =>  <li key={user}>{user}</li>) : ''}
-          </ul>
-          </Col>
-        </Row>
-        
-      </Container>
-        
-       
-    
-    </>
-  );
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/contact' element={<Cards/>}/>
+    </Routes>
+  
+  )
 }
 
 export default App;
--- src/Components/HomeMenu.js
+++ src/Components/HomeMenu.js
@@ -1,5 +1,7 @@
 import react from 'react';
 import {Row, Col} from 'react-bootstrap'
+import '../App.css'
+import {Link} from 'react-router-dom';
 
 
 function HomeMenu(props) {
@@ -8,12 +10,15 @@ function HomeMenu(props) {
   return(
     <>
     <Row className="flex-nowrap">
-        <Col md={6} sm={6} xs={6} align="left">
-        <img  className="nav-logo img-fluid" src={props.src}/> 
+       <Col md={6} sm={6} xs={6} align="left" style={{marginLeft: '.5rem'}}>
+         <img  className="nav-logo img-fluid" src={props.src}/> 
         </Col>   
-        <Col md={6} sm={6} xs={6} className="contactLink" align="right">
-          contact
-          </Col>
+        
+        <Col md={6} sm={6} xs={6} className="contactLink my-auto" align='right' style={{marginRight: '5rem'}}>
+          <Link to='/contact'>
+            contact
+          </Link>
+        </Col>
       </Row>
     </>
   )
