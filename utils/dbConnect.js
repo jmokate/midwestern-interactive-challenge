0a68c62f412d056fcacec8e3fc9e5caef7a5e0ed
jmokate <jmokate@gmail.com>
2022-06-10 14:24:07 -0500

    pg start

---
+++ utils/dbConnect.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+import {Pool} from 'pg';
+
+let connection;
+
+if (!connection) {
+    connection = new Pool({
+        user: process.env.PGSQL_USER,
+        password: process.env.PGSQL_PASSWORD,
+        host: process.env.PGSQL_HOST,
+        port: process.env.PGSQL_PORT,
+        database: process.env.PGSQL_DATABASE
+    });
+}
+
+export default connection;
